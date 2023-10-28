a9297edbf9efa90aa855c4ee69538e5d42a16887
Jimin0304 <pjws0304@naver.com>
2023-10-28 20:19:23 +0900

    ci: database 삭제

--- src/main/java/com/CSP2/switchcon/SwitchconApplication.java
+++ src/main/java/com/CSP2/switchcon/SwitchconApplication.java
@@ -2,7 +2,6 @@ package com.CSP2.switchcon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class SwitchconApplication {
--- src/main/java/com/CSP2/switchcon/security/config/SecurityConfig.java
+++ src/main/java/com/CSP2/switchcon/security/config/SecurityConfig.java
@@ -16,8 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
