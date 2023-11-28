a1f0dc50f759934f64a15c3b1906c38dcfc8f512
Jimin0304 <pjws0304@naver.com>
2023-11-29 02:54:45 +0900

    chore: cors 허용 부분 코드 추가

--- src/main/java/com/CSP2/switchcon/common/config/CorsConfig.java
+++ src/main/java/com/CSP2/switchcon/common/config/CorsConfig.java
@@ -1,2 +1,16 @@
-package com.CSP2.switchcon.common.config;public class CorsConfig {
+package com.CSP2.switchcon.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+    }
 }
