a5e5d9d95506ecb7635549599ff1c2e569ec4db1
Jimin0304 <pjws0304@naver.com>
2023-11-29 03:05:56 +0900

    chore: credentials 허용

--- src/main/java/com/CSP2/switchcon/common/config/CorsConfig.java
+++ src/main/java/com/CSP2/switchcon/common/config/CorsConfig.java
@@ -11,6 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true)
+                .allowedMethods("*");
     }
 }
