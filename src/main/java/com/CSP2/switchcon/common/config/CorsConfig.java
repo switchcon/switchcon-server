8705d59ee40b167bb7220637e952854c3224d0a3
Jimin0304 <pjws0304@naver.com>
2023-12-09 18:50:30 +0900

    chore: cors 전체허용 -> 필요한 것만 허용으로 수정

--- src/main/java/com/CSP2/switchcon/common/config/CorsConfig.java
+++ src/main/java/com/CSP2/switchcon/common/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "http://switchcon.github.io")
                 .allowCredentials(true)
                 .allowedMethods("*");
     }
